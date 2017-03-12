5b636fa7a87af325eb26785bec570fd46fe1a2f0
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-03-12 17:42:36 +0100

    Importada la carpeta de la aplicación a la del trabajo.

---
+++ BusesSevilla/app/src/main/java/com/example/alba/busessevilla/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.alba.busessevilla;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] lineas = {"M160", "M161", "M162", "M169"};
+        String [] trayectos = {"Sevilla - Pueblo", "Pueblo - Sevilla"};
+        String [] paradas = {"Pinichi", "Alambique", "Iglesia", "Blanca Paloma"};
+
+        ArrayAdapter adapterLinea = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, lineas);
+        ArrayAdapter adapterTrayecto = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, trayectos);
+        ArrayAdapter adapterParada = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, paradas);
+
+        Spinner lineaSpinner = (Spinner)findViewById(R.id.lineaSpinner);
+        Spinner trayectoSpinner = (Spinner)findViewById(R.id.trayectoSpinner);
+        Spinner paradaSpinner = (Spinner)findViewById(R.id.paradaSpinner);
+
+        lineaSpinner.setAdapter(adapterLinea);
+        trayectoSpinner.setAdapter(adapterTrayecto);
+        paradaSpinner.setAdapter(adapterParada);
+   }
+}
